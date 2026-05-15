9a3c926a2dbe0e50e4f2a24c787e46828b1a5fe6
Triinu Eesmaa <triinu.eesmaa@gmail.com>
2026-05-15 11:45:45 +0200

    is_operator variable added to Client class

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -2,6 +2,7 @@
 #define CLIENT_HPP
 
 #include <string>
+#include <stdbool.h>
 
 class Client {
     public:
@@ -15,6 +16,14 @@ class Client {
         int         fd;
         std::string username;
         std::string nickname;
+        bool        is_operator;
+        /*Nickname: max 9 chars, must be unique
+        See the protocol grammar rules
+        for what may and may not be used
+        in a nickname. In addition to the nickname, all servers must have the
+   following information about all clients: the real name of the host
+   that the client is running on, the username of the client on that
+   host, and the server to which the client is connected.*/
 
         Client();
 
--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -1,13 +1,14 @@
 #include "Client.hpp"
 
 Client::Client() {}
-Client::Client(int fd) : fd(fd), username("default_user"), nickname("default_nickname") {}
-Client::Client(const Client& original) : fd(original.fd), username(original.username), nickname(original.nickname) {}
+Client::Client(int fd) : fd(fd), username("default_user"), nickname("default_nickname"), is_operator(false) {}
+Client::Client(const Client& original) : fd(original.fd), username(original.username), nickname(original.nickname), is_operator(original.is_operator) {}
 Client& Client::operator=(const Client& other) {
     if (this != &other) {
         fd = other.fd;
         username = other.username;
         nickname = other.nickname;
+        is_operator = other.is_operator;
     }
     return *this;
 }
