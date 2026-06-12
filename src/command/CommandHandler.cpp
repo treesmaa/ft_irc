02bef69d9b1dd117239892a24f47340384c2b19e
Triinu Eesmaa <teesmaa@c2r2p8.42vienna.com>
2026-06-12 13:09:48 +0200

    feat: NOTICE command handling

--- src/command/CommandHandler.cpp
+++ src/command/CommandHandler.cpp
@@ -287,10 +287,12 @@ void CommandHandler::handleJoin(s_msg *message, Client& client) {
 }
 
 void CommandHandler::handlePrivmsg(s_msg *message, Client& client) {
-    if (message->parameters.empty()) {
+    bool automatic_reply = message->command == "NOTICE" ? false : true;
+
+    if (automatic_reply && message->parameters.empty()) {
         _server.sendToClient(client.getFd(), formReply(ERR_NORECIPIENT, message->command, client));
     }
-    if (message->parameters.size() < 2 || message->parameters[1].empty()) {
+    if (automatic_reply && (message->parameters.size() < 2 || message->parameters[1].empty())) {
         _server.sendToClient(client.getFd(), formReply(ERR_NOTEXTTOSEND, client));
         return;
     }
@@ -312,12 +314,12 @@ void CommandHandler::handlePrivmsg(s_msg *message, Client& client) {
 			std::map<std::string, Channel>& channels = _server.getChannels();
 			std::map<std::string, Channel>::iterator chan_it = channels.find(target);
 
-			if (chan_it == channels.end()) {
+			if (automatic_reply && chan_it == channels.end()) {
 				_server.sendToClient(client.getFd(), formReply(ERR_NOSUCHNICK, target, client));
 				continue;
 			}
 
-			if (!chan_it->second.hasMember(client.getFd())) {
+			if (automatic_reply && !chan_it->second.hasMember(client.getFd())) {
 				_server.sendToClient(client.getFd(), formReply(ERR_CANNOTSENDTOCHAN, target, client));
 				continue;
 			}
@@ -328,7 +330,7 @@ void CommandHandler::handlePrivmsg(s_msg *message, Client& client) {
 
 		Client* target_client = _server.getClientByNickname(target);
 
-		if (!target_client) {
+		if (automatic_reply && !target_client) {
 			_server.sendToClient(client.getFd(), formReply(ERR_NOSUCHNICK, target, client));
 			continue;
 		}
@@ -353,7 +355,7 @@ void CommandHandler::handleCommand(s_msg *message, Client& client) {
         handleQuit(message, client);
     else if (message->command == "JOIN")
         handleJoin(message, client);
-    else if (message->command == "PRIVMSG")
+    else if (message->command == "PRIVMSG" || message->command == "NOTICE")
         handlePrivmsg(message, client);
 }
 
